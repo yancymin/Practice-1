a6442e44bc094d3e08d0663058c046a53ea6c198
yancymin <senseslofter@gmail.com>
2018-01-07 21:07:39 +0800

    JS

---
+++ main.js
@@ -0,0 +1,23 @@
+// var isAdmin = true;
+// if (isAdmin) {
+//     console.log("tashi");
+// } else {
+//     console.log("dds")
+// }
+//
+// $('')
+
+var day = 3;
+switch (day) {
+    case 1:
+        console.log('11111');
+        break;
+    case 2:
+        console.log('222222');
+        break;
+    default:
+        console.log("wdsdsdsds");
+        break;
+}
+
+alert(day)
